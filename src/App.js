2a0d0c51b5c804845370a1445b3df7e429f1dfc0
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2022-01-11 12:55:28 +0500

    Boiler Plate Code Ready.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ChatEngine } from 'react-chat-engine'
+
+import "./App.css"
+import ChatFeed from './components/ChatFeed'
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID = "ebd0a309-0b45-466a-b8ed-2d6c723b5894"
+            userName = "adam"
+            userSecret = "123" //password
+            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    )
 }
 
-export default App;
+export default App
